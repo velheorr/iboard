c825e2857cbc9c84049f58af760c7ca2f3872f05
Velheorr <velheorr@gmail.com>
2024-07-17 09:46:14 +0500

    refactor eco

--- src/pages/sidebar/sideMenu.jsx
+++ src/pages/sidebar/sideMenu.jsx
@@ -23,23 +23,6 @@ const SideMenu = () => {
     const menuList = useSelector(state => state.sidemenu.menuList);
     const dispatch = useDispatch()
 
-    /*const renderList = (data)=>{
-        if (data){
-            return data.map(item => {
-                return  <Link to={`/${item.link}`} key={item.id}>
-                    <ListItem disablePadding  className={item.active ? 'active' : null}
-                              onClick={()=> dispatch(setActive(item.id))}
-                              sx={{'&:hover': {backgroundColor: mode === 'dark' ? 'rgb(81 81 81)' : ''}}}
-                    >
-                        {
-                            item.id === 8 && <SpecialId8 item={item}/>
-                        }
-                    </ListItem>
-                </Link>
-            })
-        }
-      }
-    const sidebarData = renderList(menuList)*/
     const neon = useTheme('neonGreen')
     const color = useTheme('divider')
 
@@ -96,27 +79,3 @@ const Tree = ({name, img = false, ico = false, link = false, children})=>{
     )
 }
 
-/*const SideBarList = ({item})=>{
-    return (
-        <ListItemButton sx={{height: 48,px: 2.5}}>
-            <img className='menuIcon' src={item.icon} alt={item.name}
-                 style={{filter: useTheme() ? 'brightness(0) invert(1)': null}}
-            />
-            <div>{item.name}</div>
-        </ListItemButton>
-    )
-}*/
-
-const SpecialId8 = (item) => {
-    return (
-            <ListItemButton sx={{height: 80,px: 2.5}}>
-                <img className='menuIcon' src={guardian_black} alt={item.name}
-                     style={{filter: useTheme() ? 'brightness(0) invert(1)': null}}/>
-                <div>
-                    <Typography component="div">Потери</Typography>
-                    <Typography component="div">Разрывы</Typography>
-                    <Typography component="div">Развитие</Typography>
-                </div>
-            </ListItemButton>
-    )
-}
