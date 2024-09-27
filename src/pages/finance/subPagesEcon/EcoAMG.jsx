80e719719910318f7cf4fcd5df3cb38352ced8ab
Velheorr <velheorr@gmail.com>
2024-09-27 17:14:26 +0500

    fix shcedule

--- src/pages/finance/subPagesEcon/EcoAMG.jsx
+++ src/pages/finance/subPagesEcon/EcoAMG.jsx
@@ -1,12 +1,13 @@
 import '../econ.scss'
 import AmgFunnel from "./EcoAmg/AmgFunnel";
+import AmgStackedBar from "./EcoAmg/AmgStackedBar";
 
 const EcoAmg = () => {
     return (
         <div className='amg'>
             <AmgFunnel className='amgChart'/>
-            <div className='amgChart'>2</div>
             <div className='amgChart'>3</div>
+            <AmgStackedBar className='amgChart'/>
         </div>
     );
 };
---
+++ src/pages/finance/subPagesEcon/EcoAmg/AmgStackedBar.jsx
@@ -0,0 +1,80 @@
+import React, {useMemo, useState} from 'react';
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import {chartConfig} from "../../js/chartConfig";
+
+const AmgStackedBar = ({className}) => {
+    const [isLegendVisible, setIsLegendVisible] = useState(false);
+
+
+    const options = useMemo(() => ({
+        chart: {type: 'bar', ...chartConfig.chart, height: 330,},
+        title: {text: 'Показатели нарастающим итогом', ...chartConfig.title},
+        subtitle: {text: null, ...chartConfig.subtitle},
+        legend: {enabled: isLegendVisible,...chartConfig.legend},
+        xAxis: {...chartConfig.xAxis,
+            categories: [
+                'ФОТ', 'Субподряд и Спецмеханизмы', 'Общепроизводственные', 'Ресурсные', 'Обеспечивающие', 'Накладные', 'Налоги', 'АМГ Бизнес', 'АМГ Управление',
+            ]},
+        yAxis: {...chartConfig.yAxis, },
+        lang: {...chartConfig.lang},
+        exporting: {
+            buttons: {
+                contextButton: {
+                    ...chartConfig.exporting.buttons.contextButton,
+                    menuItems: [
+                        'viewFullscreen',
+                        {
+                            text: 'Легенда',
+                            onclick: function () {
+                                setIsLegendVisible(!isLegendVisible); // Toggle legend visibility
+                            },
+                        },
+                        "printChart", "separator",
+                        "downloadPNG", "downloadJPEG", "downloadPDF",
+                    ],
+                },
+            },
+        },
+        tooltip: {
+            format: '<b>{key}</b><br/><span style="color:{series.color}">{series.name}</span>: {y} млн.<br/>' /*+ 'Total: {point.stackTotal}'*/
+        },
+        plotOptions: {
+            series: {
+                stacking: 'normal',
+                dataLabels: {
+                    enabled: true
+                }
+            }
+        },
+        series: [{
+            name: 'Потери',
+            data: [137, 137, 83, 68, 100, 122,54,45, 76]
+            },
+            {
+                name: 'Накоплено',
+                data: [137, 83, 70, 68, 100, 22,82,45, 76]
+            },
+            {
+                name: 'План на конец года',
+                data: [83, 70, 70, 68, 100, 75,92,45, 76]
+            },
+            {
+                name: 'Прогноз на конец года',
+                data: [70, 68, 70, 68, 100, 46,212,45, 76]
+            },
+        ]
+    }), [isLegendVisible])
+
+
+    return (
+        <div className={className}>
+            <HighchartsReact
+                highcharts={Highcharts}
+                options={options}
+            />
+        </div>
+    );
+};
+
+export default AmgStackedBar;
