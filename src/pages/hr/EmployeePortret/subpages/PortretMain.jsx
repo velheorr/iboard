8829b1cc1d78ae10ef49d20980e11ae7bde90435
Velheorr <velheorr@gmail.com>
2024-12-16 10:12:35 +0500

    portret age

--- src/pages/hr/EmployeePortret/subpages/PortretMain.jsx
+++ src/pages/hr/EmployeePortret/subpages/PortretMain.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import PortretSalary from "./portretbub/PortretSalary";
+import PortretAge from "./portretbub/PortretAge";
 
 const PortretMain = () => {
     return (
         <div>
+            <PortretAge/>
             <PortretSalary/>
         </div>
     );
